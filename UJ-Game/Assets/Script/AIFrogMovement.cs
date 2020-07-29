c7e9046f93e767745fb5756387592ff6186c9430
thinhphat755 <48512210+thinhphat755@users.noreply.github.com>
2020-07-29 16:07:15 +0700

    add activity (Hurt player) for Frog enemy

--- UJ-Game/Assets/Script/AIFrogMovement.cs
+++ UJ-Game/Assets/Script/AIFrogMovement.cs
@@ -1,34 +1,72 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AIFrogMovement : MonoBehaviour
 {
     private Animator animator;
-    public Transform roamingPosition;
+    public Transform target;
     [SerializeField]
     private float speed = 0f;
+    [SerializeField]
+    private float maxRange = 0f;
+    [SerializeField]
+    private float minRange = 0f;
+    public Transform homePos;
+    //public bool moveRight;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        target = FindObjectOfType<PlayerMovement>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        GoToRoamingPosition();
-        Debug.Log(Vector3.Distance(transform.position, roamingPosition.position));  
-        if (Vector3.Distance(transform.position, roamingPosition.position) < 0.85f)
+        if (Vector3.Distance(transform.position, target.position) <= maxRange 
+            && Vector3.Distance(transform.position, target.position) >= minRange)
         {
-            animator.SetBool("isJumping", false);
+            FollowPlayer();
+        }
+        else if (Vector3.Distance(transform.position, target.position) >= maxRange)
+        {
+            GoHome();
         }
     }
 
-    void GoToRoamingPosition()
+    void FollowPlayer()
     {
         animator.SetBool("isJumping", true);
-        transform.position = Vector2.MoveTowards(transform.position, roamingPosition.position, speed * Time.deltaTime);
+        if (target.position.x > transform.position.x)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            transform.localScale = new Vector2(1, 1);
+        }
+    }
+
+    public void GoHome()
+    {
+        if (homePos.position.x > transform.position.x)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, homePos.position, speed * Time.deltaTime);
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, homePos.position, speed * Time.deltaTime);
+            transform.localScale = new Vector2(1, 1);
+        }
+        if (Vector3.Distance(transform.position, homePos.position) == 0)
+        {
+            animator.SetBool("isJumping", false);
+        }
     }
 }
---
+++ UJ-Game/Assets/Script/HurtPlayer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HurtPlayer : MonoBehaviour
+{
+    private float waitToHurt = 1f;
+    private bool isTouching;
+    private HealthManage healthManage;
+    private bool reloading;
+    private float waitToLoad = 2f;
+    [SerializeField]
+    private int damageToGive = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthManage = FindObjectOfType<HealthManage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (healthManage.currentHealth <= 0)
+        {
+            reloading = true;
+        }
+        if (reloading)
+        {
+            waitToLoad -= Time.deltaTime;
+            if(waitToLoad <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+        if (isTouching)
+        {
+            waitToHurt -= Time.deltaTime;
+            if (waitToHurt <= 0)
+            {
+                healthManage.HurtPlayer(damageToGive);
+                waitToHurt = 1f;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "Player")
+        {
+            healthManage.HurtPlayer(damageToGive);
+            
+        }
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.collider.tag == "Player")
+        {
+            isTouching = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.collider.tag == "Player")
+        {
+            isTouching = false;
+            waitToHurt = 1f;
+        }
+    }
+}
