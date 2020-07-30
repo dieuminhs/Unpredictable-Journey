ba815ced8d53f6318262224a1cc33bbc52156af5
thinhphat755 <48512210+thinhphat755@users.noreply.github.com>
2020-07-30 11:21:44 +0700

    add flash effect (main character) when being hurt by enemies

--- UJ-Game/Assets/Script/HealthManage.cs
+++ UJ-Game/Assets/Script/HealthManage.cs
@@ -6,21 +6,60 @@ public class HealthManage : MonoBehaviour
 {
     public int currentHealth = 0;
     public int maxHealth = 0;
+
+    private bool flashActive;
+    [SerializeField]
+    private float flashLength = 0f;
+    private float flashCounter = 0f;
+    private SpriteRenderer playerSprite;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(flashActive)
+        {
+            if(flashCounter > flashLength * 0.99f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
+            } else if (flashCounter > flashLength * 0.82f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+            } else if (flashCounter > flashLength * 0.66f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
+            } else if (flashCounter > flashLength * 0.49f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+            } else if (flashCounter > flashLength * 0.33f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
+            } else if (flashCounter > flashLength * 0.16f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+            } else if (flashCounter > 0f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
+            }
+            else
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+                flashActive = false;
+            }
+            flashCounter -= Time.deltaTime;
+        }
     }
 
     public void HurtPlayer(int damageToGive)
     {
         currentHealth -= damageToGive;
+        flashActive = true;
+        flashCounter = flashLength;
         if(currentHealth <= 0)
         {
             gameObject.SetActive(false);
