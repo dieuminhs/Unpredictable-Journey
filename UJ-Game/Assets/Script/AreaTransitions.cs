51c1aab9cb0f963a01ab8638fc02445ed113b1e7
thinhphat755 <48512210+thinhphat755@users.noreply.github.com>
2020-08-24 17:02:03 +0700

    decorated for house scene and forest (including tilemaps for decoration)

---
+++ UJ-Game/Assets/Script/AreaTransitions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AreaTransitions : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player" && !other.isTrigger)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
