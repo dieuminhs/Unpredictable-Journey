90aead54c6bba2f0dd5a9efca78a9ce326ec9557
Lancelot <Lancelot@LAPTOP-KFO5PQBF>
2020-08-26 11:50:36 +0700

    Update Orbs, health UI

---
+++ UJ-Game/Assets/Script/Acquired.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acquired : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
